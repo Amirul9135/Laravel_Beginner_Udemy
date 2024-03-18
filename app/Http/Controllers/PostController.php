fbca86d0d4c7c9ae8b31bdd6582eeaadd8ffa8eb
Amirul Asraf <amirul.asraf9135@gmail.com>
2024-03-18 11:32:17 +0800

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -77,7 +77,7 @@ class PostController extends Controller
 
         // Your code to validate and store a new user
         //return redirect('aprofile/'.auth()->user()->username.'/posts')->with('success', 'Posted Successfully');
-        return redirect()->route('profile', ['user:username' => auth()->user()->username]);
+        return redirect()->route('profile', ['user' => auth()->user()->username]);
     }
 
     /**
